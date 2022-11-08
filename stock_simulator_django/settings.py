538f61fd156c0720a69a746543fcd1f203899b0d
Akshay Urs <84136865+akshayurs@users.noreply.github.com>
2022-11-08 21:51:37 +0530

    csrf heroku fix

--- stock_simulator_django/settings.py
+++ stock_simulator_django/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['stocksimulatordjango.herokuapp.com',
                  'localhost', '127.0.0.1', '[::1]']
-
+CSRF_TRUSTED_ORIGINS = ['https://stocksimulatordjango.herokuapp.com']
 
 # Application definition
 
